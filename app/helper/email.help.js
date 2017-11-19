f3c22fb9f96b3410e0ef6497ec3414bbc089f836
Kain Shi <kainaltion@Kains-MacBook-Pro.local>
2017-11-19 22:51:17 +0800

    _

--- app/helper/email.help.js
+++ app/helper/email.help.js
@@ -79,18 +79,25 @@ const template_email = (user, address, product_item) => {
 	+ str
 }
 
-const template = (user, email, password) => { return '<p>Dear ' + user.first_name + '</p>'
-+ '<p>Congratulations, you have made the 1st step to becoming a MYbarre Instructor.  You have been accepted into the following training course:</p>'
-+ '<p>Dates: Friday 27th, Saturday 28th, Sunday 29th October 2017</p>'
-+ '<p>Venue: Z&B Fitness, Golden Eagle Mall, 278 Shaanxi Road, 6th Floor, Shanghai. </p>'
-+ '<p>With our 3 day comprehensive Instructor Training, you will learn how to inspire, motivate, and lead others safely and effectively through the MYbarre program. It is an open learning environment delivered in person by our MYbarre Master Trainers. Delivered through a mix of practical master classes, theory workshops, small group exercises, class teaching and practice presentations.</p>'
-+ '<p>You will now have access to 2 videos on the MYbarre website.  You must spend time before the training weekend watching and learning these. You will be asked to present the warm up (along with other things you will learn) on the training.</p>'
-+ '<p>Basic Moves of MYbarre</p>'
-+ '<p>5 minute MYbarre warm up</p>'
-+ '<p>Please revisit our website and login using the following password: ' + password + '</p>'
-+ '<p>We will email you with more specific information about the weekend shortly, in the mean time please drop us an email with any questions.  We look forward to meeting you soon.</p>'
-+ '<p>Kind regards,</p>'
-+ '<p>MYbarre Master Training Team</p>';
+const template = (user, email, password) => { return '<p>Dear '+ user.first_name + ' ' + user.last_name + '</p>'
++'<p>Congratulations! You have now registered with MYbarre.</p>'
++'<p>You can now revisit the MYbarrefitness website and click on the INSTRUCTOR LOGIN (top right of page) using the following password: '+password+'</p>'
++'<p>If you have signed up for the Instructor Training course we will be in touch shortly with more specific information.</p>'
++'<p>Kind regards,</p>'
++'<p>MYbarre Fitness Team</p>';
+
+// 	'<p>Dear ' + user.first_name + '</p>'
+// + '<p>Congratulations, you have made the 1st step to becoming a MYbarre Instructor.  You have been accepted into the following training course:</p>'
+// + '<p>Dates: Friday 27th, Saturday 28th, Sunday 29th October 2017</p>'
+// + '<p>Venue: Z&B Fitness, Golden Eagle Mall, 278 Shaanxi Road, 6th Floor, Shanghai. </p>'
+// + '<p>With our 3 day comprehensive Instructor Training, you will learn how to inspire, motivate, and lead others safely and effectively through the MYbarre program. It is an open learning environment delivered in person by our MYbarre Master Trainers. Delivered through a mix of practical master classes, theory workshops, small group exercises, class teaching and practice presentations.</p>'
+// + '<p>You will now have access to 2 videos on the MYbarre website.  You must spend time before the training weekend watching and learning these. You will be asked to present the warm up (along with other things you will learn) on the training.</p>'
+// + '<p>Basic Moves of MYbarre</p>'
+// + '<p>5 minute MYbarre warm up</p>'
+// + '<p>Please revisit our website and login using the following password: ' + password + '</p>'
+// + '<p>We will email you with more specific information about the weekend shortly, in the mean time please drop us an email with any questions.  We look forward to meeting you soon.</p>'
+// + '<p>Kind regards,</p>'
+// + '<p>MYbarre Master Training Team</p>';
 }
 
  module.exports = help
